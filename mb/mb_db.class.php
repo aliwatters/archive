186aa86c884ad44e7de6fbb5b0007abdd0da87f9
Mark Smalley <mark@smalley.my>
2011-10-10 19:29:36 +0800

    tidy-up of code :p - am sure ali will not like it :-)

--- mb/mb_db.class.php
+++ mb/mb_db.class.php
@@ -9,55 +9,48 @@ class MONGOBASE_DB extends MONGOBASE {
     
     function __construct(){
         parent::__construct();
-
-        $this->is_connected = $this->connect(); // probably not to use here but on the queries
+        $this->is_connected = $this->connect();
     }
 
 	private function connect(){
-		/*
-		For authentication and security, mongodb need to execute/run with --auth parameter
-		Reference: http://www.mongodb.org/display/DOCS/Security+and+Authentication
-		*/
 
         if (is_object($this->dbh)) return $this->dbh;
-
         $options = $this->options();
+
 		try{
-			if ( $options['db_replicas'] === true && $options['db_user'] !=='' ) {
-				
+
+			if($options['db_replicas'] === true && $options['db_user'] !=='' ){
                 //replica and database need authentication
 				$m = new Mongo("mongodb://{$options['db_user']}:{$options['db_pass']}@{$options['db_host']}:{$options['db_port']}/{$options['db_name']}", array('replicaSet' => true));
-			} elseif ($options['db_replicas'] === true) {
+			}elseif($options['db_replicas'] === true){
 				//replica set and no auth.
 				$m = new Mongo("mongodb://{$options['db_host']}:{$options['db_port']}/{$options['db_name']}", array('replicaSet' => true));
-
-			} elseif ($options['db_user']!=false) { //database need authentication
+			}elseif($options['db_user']!=false){
+				////database need authentication
 				$m = new Mongo("mongodb://{$options['db_user']}:{$options['db_pass']}@{$options['db_host']}:{$options['db_port']}/{$options['db_name']}");
-
-			} else {
+			}else{
 				//default without auth and replica
 				$m = new Mongo("mongodb://{$options['db_host']}:{$options['db_port']}/{$options['db_name']}");
-
 			}
+
             $this->mongo = $m;
             $this->dbh = $m->db;
 			return true;
-		} catch (MongoConnectionException $e) {
-            $this -> error = $this->__('Error connecting to MongoDB server');
-        } catch (MongoException $e) {
-            $this -> error = $this->__('Error: mongoDB error').$e->getMessage();
-        } catch (MongoCursorException $e) {
-            $this -> error = $this->__('Error: probably username password in config').$e->getMessage();
-        } catch (Exception $e) {
-            $this -> error = $this->__('Error: ').$e->getMessage();
+
+		}catch(MongoConnectionException $e){
+            $this->error = $this->__('Error connecting to MongoDB server');
+        }catch(MongoException $e) {
+            $this->error = $this->__('Error: mongoDB error').$e->getMessage();
+        }catch(MongoCursorException $e) {
+            $this->error = $this->__('Error: probably username password in config').$e->getMessage();
+        }catch(Exception $e) {
+            $this->error = $this->__('Error: ').$e->getMessage();
         }
-        
         return false;
 	}
 
-
 	private function arrayed($these_objs){
-    /* takes mongo_db objects and returns them as arrays for php */
+		/* takes mongo_db objects and returns them as arrays for php */
         if(is_object($these_objs)){
             $objects = array();
             foreach($these_objs as $this_obj) {
@@ -101,18 +94,18 @@ class MONGOBASE_DB extends MONGOBASE {
         
     }
 
-
-	/* mbsert() allow for intelligent inserting and (or) updating */
 	public function mbsert($options = false){
+
+		/* mbsert() allow for intelligent inserting and (or) updating */
 
 		$defaults = array(
             'col'	=> 'mbsert',
             'obj'   => false,
 			'id'	=> false
         );
-
         $settings = $this->settings($options,$defaults);
-        if (! $this->is_connected) $this->connect();
+
+        if(!$this->is_connected) $this->connect();
         $dbh = $this->dbh; 
 
 		try{
@@ -131,13 +124,14 @@ class MONGOBASE_DB extends MONGOBASE {
 			) );
 			return $results['value']['_id'];
 
-		} catch (Exception $e) {
+		}catch(Exception $e){
             // should be able to do this class wide on the base object
             return $this->__('Error: ').$e->getMessage();
         }
 	}
 
 	public function find($options = false){
+
 		$defaults = array(
             'col'		=> 'mbsert',
             'where'		=> array(),
@@ -147,27 +141,25 @@ class MONGOBASE_DB extends MONGOBASE {
 			'order'		=> false,
 			'id'		=> false
         );
-
         $settings = $this->settings($options,$defaults);
         
         if($settings['order_by']){
             if ($settings['order']!='desc') $order_value=1; else $order_value=-1; 
 			$sort_clause = array($order_by=>$order_value);
-		} else {    
+		}else{    
             $sort_clause = array(); 
         }
 
-        if (! $this->is_connected) $this->connect();
+        if(!$this->is_connected) $this->connect();
         $dbh = $this->dbh;
 
 		try{
 
 			$collection = $dbh->$settings['col'];
-
 			$results = $this->arrayed($collection->find($settings['where'])->sort($sort_clause)->skip($settings['offset'])->limit($settings['limit']));
 			return $results;
 
-        } catch (Exception $e) {
+        }catch(Exception $e){
             return $this->__('Error: ').$e->getMessage();
         }
 	}
@@ -178,24 +170,21 @@ class MONGOBASE_DB extends MONGOBASE {
             'col'		=> 'mbsert',
 			'id'		=> false
         );
-
         $settings = $this->settings($options,$defaults);
 
-        if (! $this->is_connected) $this->connect();
+        if(!$this->is_connected) $this->connect();
         $dbh = $this->dbh;
-
 
 		try{
 
 			$collection = $dbh->$settings['col'];
-
 			$criteria = array(
 				'_id' => new MongoId($settings['id']),
 			);
 			$progress = $collection->remove($criteria,array('safe'=>true));
 			return $progress['n'];
 
-		} catch (Exception $e) {
+		}catch(Exception $e){
             return $this->__('Error: ').get_class($e).' : '.$e->getMessage();
         }
 	}
