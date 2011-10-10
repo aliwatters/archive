74254a289deace0914a5afdd6072a00fa4285de8
Mark Smalley <mark@smalley.my>
2011-10-10 12:58:08 +0800

    adding source

---
+++ mb/mb.php
@@ -0,0 +1,6 @@
+<?php
+require_once(dirname(__FILE__).'/mb_config.php');
+require_once(dirname(__FILE__).'/mb_plugins.php');
+require_once(dirname(__FILE__).'/mb_lang.php');
+require_once(dirname(__FILE__).'/mb_utils.php');
+require_once(dirname(__FILE__).'/mb_class.php');
